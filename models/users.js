9046347dcb6fee15219e653652f522313deaab98
Prasoon Rajpoot <betusingh890@gmail.com>
2022-10-07 20:15:56 +0530

    fadf

---
+++ models/users.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const UserScheema = mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    email: {
+        type : String,
+        required : true
+    },
+    password: {
+        type : String,
+        required: true
+    },
+    contact : {
+        type : Number,
+        required : true
+    },
+    joining : {
+        type : Date,
+        required : true
+    },
+    department : {
+        type : String,
+        required : true
+    },
+
+    admin : {
+        type : Boolean,
+        default : false,
+    }
+});
+
+
+const Employees = mongoose.model("Employees", UserScheema);
+
+export default Employees;
