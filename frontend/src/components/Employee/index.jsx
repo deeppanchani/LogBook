52bea6c1d72a9207b8f0f948ad9b0024b48a505c
Prasoon Rajpoot <betusingh890@gmail.com>
2022-10-08 23:02:15 +0530

    added login verification for employee dashboard

--- frontend/src/components/Employee/index.jsx
+++ frontend/src/components/Employee/index.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import "./index.scss"
-
+import {useSelector} from "react-redux"
 import AddTaskButton from "./addTask";
+
 
 function EmployeeDashboard(){
     var [addTaskShow, setAddTask] = React.useState(false);
+    var isLoggedIn = useSelector((state) => state.isLoggedIn);
 
     function handleEdit(){
         setAddTask(!addTaskShow);
     }
 
-    return (
+    if(isLoggedIn == false){
+        return (
+            <div>
+                Please Login to Be here
+            </div>
+        )
+    }
+    else return (
         <div className="employee-dashboard">
             <div className="top-employee">
                 <h3>Today's Tasks</h3>
