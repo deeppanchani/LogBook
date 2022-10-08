f819c89bbcc78ec95dd8435ddb48bcb45d8fa8b6
Yuvrajsinh Kamalia <yuvrajsinhkamalia28@gmail.com>
2022-10-08 17:05:27 +0530

    form structure

--- frontend/src/components/admin/addEmployee.jsx
+++ frontend/src/components/admin/addEmployee.jsx
@@ -4,14 +4,29 @@ function AddEmployee(){
     return (
         <div>
             <form action="">
-                <label htmlFor="">Name</label>
+                <label htmlFor="name">Name</label>
+                <br/>   
                 <input type="text" />
-                <label htmlFor=""></label>
+                <br/>   
+                <label htmlFor="email">Email</label>
+                <br/>   
+                <input type="email" />
+                <br/>   
+                <label htmlFor="contact">Contact Number</label>
+                <br/>   
+                <input type="tel" />
+                <br/>   
+                <label htmlFor="department">Department</label>
+                <br/>   
                 <input type="text" />
-                <label htmlFor=""></label>
-                <input type="text" />
-                <label htmlFor=""></label>
-                <input type="text" />
+                <br/>   
+                <label htmlFor="joining">Joining Date</label>
+                <br/>   
+                <input type="date" />
+                <br/>   
+                <label htmlFor="password">Password</label>
+                <br/>   
+                <input type="password" />
             </form>
         </div>
     )
