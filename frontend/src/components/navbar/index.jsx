5721ee123c83e133fa146bcd0eb9fa454197e1f9
Prasoon Rajpoot <betusingh890@gmail.com>
2022-10-08 23:18:40 +0530

    added logged in options

--- frontend/src/components/navbar/index.jsx
+++ frontend/src/components/navbar/index.jsx
@@ -3,9 +3,26 @@ import blackLogo from "./Logo_Dark_Theme.svg";
 import lightLogo from "./Logo_Light_Theme.svg"
 import "./index.scss"
 
+import {useSelector, useDispatch} from "react-redux"
+import {useNavigate} from "react-router-dom"
 import {MdDarkMode, MdLightMode} from "react-icons/md"
 
+import {LogOutAction} from "../actions.js"
 function Navbar(){
+
+    var isLoggedIn = useSelector((state) => state.isLoggedIn);
+    const dispatch = useDispatch();
+    const navi = useNavigate();
+    function handleLogClick(event){
+      event.preventDefault();
+      if(isLoggedIn){
+        // console.log(" we here ");
+        dispatch(LogOutAction());
+      }else{
+        navi("/");
+      }
+    }
+
     return (
       <nav className="nav">
         <div className="logos">
@@ -14,8 +31,9 @@ function Navbar(){
             <h2>LogBook</h2>
           </div>
         </div>
-        <div className="display-mode">  
+        <div className="display-mode">
           <MdDarkMode className="mode-button" size={35.75}></MdDarkMode>
+          <button onClick={handleLogClick} className="LogButton">{isLoggedIn ? "LogOut" : "LogIn"}</button>
         </div>
       </nav>
     );
