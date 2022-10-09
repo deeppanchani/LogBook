7a34bd3c451f9990c972c0d55b5e10dcb5eff5ee
Deep Panchani <dlplearning00@gmail.com>
2022-10-09 19:37:09 +0530

    Test

--- frontend/src/components/Employee/index.jsx
+++ frontend/src/components/Employee/index.jsx
@@ -1,76 +1,57 @@
 import React from "react";
-import "./index.scss"
-import {useSelector} from "react-redux"
+import "./index.scss";
+import { useSelector } from "react-redux";
 import AddTaskButton from "./addTask";
 import axios from "axios";
 import AllTasks from "./TasksTable";
 import EditProfile from "./editProfile";
+import PieChart from "./PieChart";
+import BarChart from "./StackedBarChart";
 
-function EmployeeDashboard(){
-    var [addTaskShow, setAddTask] = React.useState(false);
-    var [editProfileShow, setEditProfile] = React.useState(false);
-    var isLoggedIn = useSelector((state) => state.isLoggedIn);
-    var email = useSelector((state) => state.Email);
-    var [allTasks, setAllTasks] = React.useState([]);
+function EmployeeDashboard() {
+  var [addTaskShow, setAddTask] = React.useState(false);
+  var [editProfileShow, setEditProfile] = React.useState(false);
+  var isLoggedIn = useSelector((state) => state.isLoggedIn);
+  var email = useSelector((state) => state.Email);
+  var [allTasks, setAllTasks] = React.useState([]);
 
-    React.useEffect(() => {
-        getAllTasks();
-    }, [])
+  React.useEffect(() => {
+    getAllTasks();
+  }, []);
 
-    const getAllTasks =  async () => {
-        var data = {email};
-        var response = await axios.post("/getTasksForEmployee", data)
-        // console.log(response);
-        setAllTasks(response.data);
-    }
+  const getAllTasks = async () => {
+    var data = { email };
+    var response = await axios.post("/getTasksForEmployee", data);
+    // console.log(response);
+    setAllTasks(response.data);
+  };
 
-    function handleEdit(){
-        setAddTask(!addTaskShow);
-    }
-
-    function handlePress() {
-      setEditProfile(!editProfileShow);
-    }
-
-    if(isLoggedIn == false){
-        return (
-            <div>
-                Please Login to Be here
-            </div>
-        )
-    }
-
-    
-
-    // function 
-
-    else return (
-        <div>
-          <div className="employee-dashboard">
-            <div className="top-employee">
-              <h3>Today's Tasks</h3>
-              <button className="edit-button" onClick={handlePress}>
-                {" "}
-                Edit Profile
-              </button>
-              <button className="add-button" onClick={handleEdit}>
-                + Add Task
-              </button>
-            </div>
-            {/* <div className="statGraphDaily">
-                  <PieChart charData  />
-              </div> */}
-            {/* <br />
-              <div className="weeklyGraph">
-                  <BarChart charData  />
-              </div> */}
-           
-          </div>
-          <AllTasks props={allTasks} />
-          {editProfileShow && <EditProfile props = {handlePress}/>}
-          {addTaskShow && <AddTaskButton props = {handleEdit}/>}
-        </div>
-      );
+  function handleEdit() {
+    setAddTask(!addTaskShow);
   }
-  
-  export default EmployeeDashboard;
+
+  function handlePress() {
+    setEditProfile(!editProfileShow);
+  }
+
+  if (isLoggedIn == false) {
+    return <div>Please Login to Be here</div>;
+  }
+
+  // function
+  else
+    return (
+      <main>
+        <div className="employee-dashboard">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        {editProfileShow && <EditProfile props={handlePress} />}
+        {addTaskShow && <AddTaskButton props={handleEdit} />}
+      </main>
+    );
+}
+
+export default EmployeeDashboard;
