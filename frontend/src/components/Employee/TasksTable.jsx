7255de599a1873152be517886c0236c34307d716
Prasoon Rajpoot <betusingh890@gmail.com>
2022-10-09 16:02:55 +0530

    added files for tasks table

---
+++ frontend/src/components/Employee/TasksTable.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function AllTasks(props){
+
+    var data = props;
+    console.log(props.props);
+
+    return (
+        <div>
+            {/* we in all tasks */}
+        </div>
+    )
+}
+
+export default AllTasks;
--- sourceFiles/getTasksForEmployees.js
+++ sourceFiles/getTasksForEmployees.js
@@ -1,4 +1,4 @@
-import { Express } from "express";
+import Express from "express";
 import bodyParser from "body-parser";
 import Tasks from "../models/tasks.js";
 
@@ -8,10 +8,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
+
 app.post("/", (req, res) => {
     var data = req.body;
     
-    
+    console.log(data);
+
+    Tasks.find({email : data.email}, function(err,result){
+        if(err){
+            console.log("we got error");
+            console.log(err);
+            res.send("Error");
+        }else{
+            console.log(result);
+            res.send(result);
+        }
+    })
 
 })
 
