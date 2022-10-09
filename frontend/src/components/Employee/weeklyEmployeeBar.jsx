dd733ad75f7856a97ed49d12022c1852ec9e2432
Yuvrajsinh Kamalia <yuvrajsinhkamalia28@gmail.com>
2022-10-09 23:14:07 +0530

    nothing

---
+++ frontend/src/components/Employee/weeklyEmployeeBar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import BarChart from "./StackedBarChart";
+
+function WeeklyEmployeeBar(props){
+    var data = props.props;
+    var totalWork = []
+    var totalMeet = []
+    var totalBreak = []
+    var pastDates = []
+    for(var i=6;i>=0;i++){
+        var work = 0;
+        var meet = 0;
+        var recess = 0;
+        var today = new Date();
+        today.setDate(today.getDate() - i);
+        var dd = String(today.getDate()).padStart(2, "0");
+        var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+        var yyyy = today.getFullYear();
+
+        today = yyyy + "-" + mm + "-" + dd;
+
+        console.log(today);
+
+        // console.log("today is " + today);
+        for (var i = 0; i < data.length; i++) {
+            console.log("we in here ");
+            var current = data[i];
+            console.log(current);
+            var dateloc = current.start.toString();
+            dateloc = dateloc.slice(0, 10);
+            // console.log("first is " + dateloc)
+            if (dateloc == today) {
+                if (current.type == "Meeting") {
+                meet += current.duration;
+                } else if (current.type == "Work") {
+                work += current.duration;
+                } else {
+                recess += current.duration;
+                }
+                console.log("we in equal");
+            }
+            // console.log(typeof dateloc)
+            // if(dateloc ==)
+            // if()
+            totalBreak.push(recess);
+            totalMeet.push(meet);
+            totalWork.push(work);
+            pastDates.push(today);
+            console.log(current);
+        }
+    }
+    var toPass= [pastDates, totalWork, totalMeet, totalBreak]
+    return (
+        <BarChart props = {toPass}/>
+    )
+}
+
+export default WeeklyEmployeeBar;
