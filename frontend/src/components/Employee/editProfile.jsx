54223ce2558935542a14b595d76715cebcf6684c
Deep Panchani <dlplearning00@gmail.com>
2022-10-10 09:46:59 +0530

    CSS for Modal added

--- frontend/src/components/Employee/editProfile.jsx
+++ frontend/src/components/Employee/editProfile.jsx
@@ -25,39 +25,41 @@ function EditProfile(props){
     }
     
     return(
-        <div className="addTask">
-        <span><h2>Edit Profile</h2></span>
-        <form action="" className="button">
-          <label htmlFor="name">Name</label>
-          <br />
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <br />
-          <label htmlFor="contact">Contact Number</label>
-          <br />
-          <input
-            type="tel"
-            id="contact"
-            value={contact}
-            onChange={(e) => setContact(e.target.value)}
-          />
-          <br />
-          <label htmlFor="password">Password</label>
-          <br />
-          <input
-            type="text"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+      <div className="modal">
+          <div className="addTask">
+          <span><h2>Edit Profile</h2></span>
+          <form action="" className="button">
+            <label htmlFor="name">Name</label>
             <br />
-          <button onClick = {updateData} className="button"> Update Profile </button>
-          <br />
-        </form>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <br />
+            <label htmlFor="contact">Contact Number</label>
+            <br />
+            <input
+              type="tel"
+              id="contact"
+              value={contact}
+              onChange={(e) => setContact(e.target.value)}
+            />
+            <br />
+            <label htmlFor="password">Password</label>
+            <br />
+            <input
+              type="text"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+              <br />
+            <button onClick = {updateData} className="button"> Update Profile </button>
+            <br />
+          </form>
+        </div>
       </div>
     );
 }
